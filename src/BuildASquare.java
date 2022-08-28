4276684218169cfdf12986ecb5c9d136ee54841e
DeKaPoDa <yrap@yandex.ru>
2022-08-28 14:49:38 +0300

    28-08-22

---
+++ src/BuildASquare.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class BuildASquare {
+    public static final String generateShape(int n) {
+
+
+
+        }
+        //return Arrays.toString(chars);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(generateShape(3));
+        System.out.println(generateShape(5));
+        System.out.println(generateShape(8));
+    }
+}
